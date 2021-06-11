1cf53d3145f2b3a5846fcefc9732320ee394fffc
Sumanjit <sumanjit.gill11@gmail.com>
2021-06-11 01:15:58 +0000

    Add ArrayList of strings containing 3 random facts and Modify doGet() function in FactServlet.java to return the ArrayList as a JSON string

--- portfolio/src/main/java/com/google/sps/servlets/FactServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FactServlet.java
@@ -1,10 +1,14 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
 
 /** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
 @WebServlet("/fact")
@@ -12,7 +16,21 @@ public class FactServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("In 2012, a friend and I built a giant Nintendo DS and it was put on display in our school's library.");
+    // Create ArrayList of random facts
+    ArrayList<String> facts = new ArrayList<String>();
+    facts.add("In 2012, a friend and I built a giant Nintendo DS.");
+    facts.add("When I was 5 years old, I tried to do 'gymnastics' at home and broke my right arm.");
+    facts.add("I am a big Selena Gomez fan and was lucky enough to see her perform in 2010 and 2016.");  
+    
+    String json = convertToJson(facts);
+    
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
+  }
+
+  private String convertToJson(ArrayList<String> data) {
+      Gson gson = new Gson();
+      String json = gson.toJson(data);
+      return json;
   }
 }
